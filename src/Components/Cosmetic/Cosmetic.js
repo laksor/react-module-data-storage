21c15cf1aefd8fc0e72909c7ab20a240821bf2d1
laskor <al.moksud@gmail.com>
2022-03-24 23:14:56 -0400

    event handler

--- src/Components/Cosmetic/Cosmetic.js
+++ src/Components/Cosmetic/Cosmetic.js
@@ -2,15 +2,16 @@ import React from 'react';
 import './Cosmetic.css'
 
 const Cosmetic = (props) => {
-    const {name, price} = props.cosmetic;
-    const addToCart = () =>{
-        
+    const {name, price, _id} = props.cosmetic;
+    const addToCart = (_id) =>{
+        console.log(_id);
     }
     return (
         <div className='product'>
             <h1>Buy this: {name}</h1>
-            <p>price: {price}</p>
-            <button onClick={addToCart} >Add to cart</button>
+            <p>price: ${price}</p>
+            <p>id: {_id}</p>
+            <button onClick={() => addToCart(_id)} >Add to cart</button>
         </div>
     );
 };
