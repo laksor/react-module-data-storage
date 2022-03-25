846a6c1c1a3bb39e4cf205e301922561c536bb51
laskor <al.moksud@gmail.com>
2022-03-24 21:26:21 -0400

    components added

---
+++ src/Components/Cosmetics/Cosmetics.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Cosmetics.css';
+const Cosmetics = () => {
+    return (
+        <div>
+            <h1>Cosmetics store</h1>
+        </div>
+    );
+};
+
+export default Cosmetics;
