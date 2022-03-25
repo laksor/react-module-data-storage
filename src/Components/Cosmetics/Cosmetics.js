5f778e6f2787f988a91c1929d29aac8ab48addaf
laskor <al.moksud@gmail.com>
2022-03-24 22:17:57 -0400

    fake json data

--- src/Components/Cosmetics/Cosmetics.js
+++ src/Components/Cosmetics/Cosmetics.js
@@ -1,14 +1,13 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Cosmetic from '../Cosmetic/Cosmetic';
 import './Cosmetics.css';
 const Cosmetics = () => {
-    const cosmetics = [
-        {id: 1, name: 'alta', price: 5000},
-        {id: 2, name: 'blta', price: 6000},
-        {id: 3, name: 'clta', price: 7000},
-        {id: 4, name: 'dlta', price: 8000},
-        {id: 5, name: 'klta', price: 9000},
-    ]
+    const [cosmetics, setCosmetics] = useState([]);
+    useEffect( () => {
+        fetch('data.json')
+            .then(res => res.json())
+            .then(data => setCosmetics(data))
+    }, [])
     return (
         <div>
             <h1>Cosmetics store</h1>
