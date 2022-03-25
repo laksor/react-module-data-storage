a3d0528130326950fb27dd09a505935186a60fed
laskor <al.moksud@gmail.com>
2022-03-24 21:44:34 -0400

    relative path

--- src/Components/Cosmetics/Cosmetics.js
+++ src/Components/Cosmetics/Cosmetics.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { add } from '../../utilities/calculate';
 import './Cosmetics.css';
 const Cosmetics = () => {
+    const first = 55;
+    const second = 55;
+    const total = add(first, second);
     return (
         <div>
             <h1>Cosmetics store</h1>
+            <p>total: {total}</p>
         </div>
     );
 };
